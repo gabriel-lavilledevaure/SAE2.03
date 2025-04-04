940d56f7a2492fa32779d91a39a32a53352bf4ec
gabriel-lavilledevaure <gabriel.lavilledevaure@etu.unilim.fr>
2025-04-05 00:33:16 +0200

    replace du name par l'id

--- server/model.php
+++ server/model.php
@@ -33,14 +33,14 @@ function getMovie(){
     return $res; // Retourne les résultats
 }
 
-function getMovieinfos($name){
+function getMovieinfos($id){
     // Connexion à la base de données
     $cnx = new PDO("mysql:host=".HOST.";dbname=".DBNAME, DBLOGIN, DBPWD);
     // Requête SQL pour récupérer les informations du film en fonction du nom
     $sql = "SELECT Movie.id, Movie.name, image, description, director, year, Category.name AS category_name, min_age, trailer 
             FROM Movie 
             INNER JOIN Category ON Movie.id_category = Category.id 
-            WHERE LOWER(Movie.id) = LOWER(:id)
+            WHERE Movie.id = :id
 ";
 
     // Préparation de la requête SQL
