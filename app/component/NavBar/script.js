5cc089d68e89866fcf42eb4b226198c8e881e665
gabriel-lavilledevaure <gabriel.lavilledevaure@etu.unilim.fr>
2025-04-07 00:18:14 +0200

    Front Itération 6 (il manque le choix par préférence (âge restreint) ainsi que le changement de photo de profil)

--- app/component/NavBar/script.js
+++ app/component/NavBar/script.js
@@ -3,9 +3,22 @@ let template = await templateFile.text();
 
 let NavBar = {};
 
-NavBar.format = function (hAbout, hHome) {
+NavBar.format = function (hAbout, profiles) {
   let html = template;
   html = html.replace("{{hAbout}}", hAbout);
+
+  let options = profiles
+    .map(
+      (p) =>
+        `<option value="${p.name}" data-img="${p.image}">${p.name}</option>`
+    )
+    .join("");
+
+  let image = profiles[0]?.image || "";
+
+  html = html.replace("{{options}}", options);
+  html = html.replace("{{image}}", image);
+
   return html;
 };
 
---
+++ app/data/dataProfile.js
@@ -0,0 +1,44 @@
+// URL où se trouve le répertoire "server" sur mmi.unilim.fr
+let HOST_URL = "https://mmi.unilim.fr/~lavilledevaur1/SAE/SAE2.03";
+
+let DataProfile = {};
+
+// DataProfile.requestMovies = async function () {
+//   // Récupération des films
+//   let answer = await fetch(HOST_URL + "server/script.php?todo=getMovie");
+//   let movies = await answer.json();
+//   return movies;
+// };
+
+DataProfile.requestProfiles = async function () {
+  let answer = await fetch(HOST_URL + "/server/script.php?todo=getProfiles");
+  let profiles = await answer.json();
+  return profiles;
+};
+
+// DataProfile.requestMoviedetails = async function (id) {
+//   let answer = await fetch(
+//     HOST_URL + "/server/script.php?todo=getMovieinfos&id=" + id
+//   );
+//   let movie = await answer.json();
+//   return movie;
+// };
+
+// DataProfile.requestCategories = async function () {
+//   let answer = await fetch(HOST_URL + "/server/script.php?todo=getCategories");
+//   let categories = await answer.json();
+//   return categories;
+// };
+
+// DataProfile.requestMoviecategorie = async function (categorie) {
+//   let answer = await fetch(
+//     HOST_URL +
+//       "/server/script.php?todo=getMoviecategorie&categorie=" +
+//       categorie
+//   );
+//   let movie = await answer.json();
+//   return movie;
+// };
+
+// On exporte la fonction DataProfile.requestMovies
+export { DataProfile };
