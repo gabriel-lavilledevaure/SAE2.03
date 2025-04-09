332f24e6e12f9ec54c50bb4a01203537c48126ea
gabriel-lavilledevaure <gabriel.lavilledevaure@etu.unilim.fr>
2025-04-09 16:16:33 +0200

    Itération 10 Fix

--- app/component/Movie/script.js
+++ app/component/Movie/script.js
@@ -11,17 +11,26 @@ Movie.format = function (movies, isLikesPage = false) {
   let html = "";
   movies.forEach((movie) => {
     let movieHtml = template;
+
+    // Remplacements principaux
     movieHtml = movieHtml.replace("{{titre}}", movie.name);
     movieHtml = movieHtml.replace("{{image}}", movie.image);
     movieHtml = movieHtml.replace("{{id}}", movie.id);
+    movieHtml = movieHtml.replace("{{id}}", movie.id); // deuxième remplacement pour l'autre bouton
     movieHtml = movieHtml.replace(
       "{{handlerFavoris}}",
       `C.handlerAddFavoris(${movie.id}, this)`
     );
     movieHtml = movieHtml.replace(
-      "{{handlerRemoveFavoris}}",
-      `C.handlerRemoveFavoris(${movie.id}, this)`
+      "{{deleteHandler}}",
+      `<button
+         class="card__delete"
+         data-id="${movie.id}"
+         onclick="event.stopPropagation(); C.handlerDeleteLike(${movie.id}, this);"
+         title="Supprimer des likes"
+       >✖</button>`
     );
+
     movieHtml = movieHtml.replace(
       "{{handler}}",
       `C.handlerDetail(${movie.id}, this)`
